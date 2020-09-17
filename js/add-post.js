dd6969269846910b7601b468c7fe8bcb1b5f9885
Mallvin2000 <mallvin2000@gmail.com>
2020-09-17 19:00:17 +0800

    add,get post API.add post page with filename nmore

---
+++ js/add-post.js
@@ -0,0 +1,62 @@
+
+function checkImages() {
+    var file = $('#imageFile')[0].files;
+    console.log(file);
+    var totalFileSize = 0;
+    var imagesNames = [];
+
+    for (let i = 0; i < file.length; i++) {
+        console.log(file[i].name);
+        imagesNames.push(file[i].name);
+        
+    }
+
+    return imagesNames
+}
+
+
+
+function sendDataToBackend(event) {
+    event.preventDefault();
+    //var images = ["abc", "123"];
+    /* var content = ``;
+    content += $('#txt_content').val();
+ */
+
+    var images = checkImages();
+
+    var settings = {
+        "url": "http://localhost:3000/insert/post",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        "data": {
+            "title": $('#txt_title').val(),
+            "date": $('#txt_date').val(),
+            "author": $('#txt_author').val(),
+            "content": $('#txt_content').val(),
+            "images": images
+        }
+    };
+
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        alert("Added sucessfully")
+    })
+        .fail((response) => {
+            alert("ERROR");
+        });
+
+
+}
+
+function registerAddPostForm() {
+    $('#add-post-form').submit(sendDataToBackend);
+}
+
+
+$(document).ready(function () {//run when document is populated
+    registerAddPostForm();
+});
