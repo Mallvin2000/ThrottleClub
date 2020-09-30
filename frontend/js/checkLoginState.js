73ca9b0f42ac3e39a81161e253732e13afa1202d
Mallvin2000 <mallvin2000@gmail.com>
2020-09-30 16:10:51 +0800

    handling message and error for contact me page

--- frontend/js/checkLoginState.js
+++ frontend/js/checkLoginState.js
@@ -20,7 +20,6 @@ function showLoggedInNavBar() {
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="admin-dashboard.html">Admin Dashboard</a></li>
-                <li class="nav-item"><a class="nav-link" href="add-post.html">Add post</a></li>
                 <li class="nav-item"><a class="nav-link" href="create-account.html">Create Account</a></li>
                 <li class="nav-item"><a class="nav-link" href="login.html">Login</a></li>
                 <li class="nav-item"><a class="nav-link" href="logout.html">Logout</a></li>
@@ -43,6 +42,7 @@ function showNotLoggedInNavBar() {
         <div class="nav-list collapse navbar-collapse" id="collapsibleNavbar">
             <ul class="navbar-nav">
                 <li class="nav-item"><a class="nav-link" href="index.html">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact-me.html">Contact Me</a></li>
             </ul>
         </div>
     `;
